07ba3e6000de85fd83102ae60964337feb953d76
Nur Hanis Damia Azmi <miawork127@gmail.com>
2025-07-31 13:18:02 +0800

    Initial commit

---
+++ app/index.tsx
@@ -0,0 +1,14 @@
+import { Text, View } from "react-native";
+
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        alignItems: "center",
+      }}
+    >
+      <Text>Damia Azmi</Text>
+    </View>
+  );
+}
